ceac86051b6fd460cc67cd6e2cc2e4105a7a4eaa
Kim <1074804240@qq.com>
2019-12-20 14:42:22 +0800

    aaaaaaaaa

--- src/main/java/aaaa.java
+++ src/main/java/aaaa.java
@@ -1,10 +1,5 @@
 public class aaaa {
     public static void main(String[] args) {
         System.out.println("aaaaaaaa11111111");
-        System.out.println("bbbbbbbb");
-        System.out.println("ccccccc");
-        System.out.println("ddddd");
-        System.out.println("eee");
-        System.out.println("ffffffff");
     }
 }

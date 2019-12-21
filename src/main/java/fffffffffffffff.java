7da36990310254881141d9c91a4466cca6165894
Kim <1074804240@qq.com>
2019-12-21 09:55:52 +0800

    fffffffffffffff

--- src/main/java/fffffffffffffff.java
+++ src/main/java/fffffffffffffff.java
@@ -1,5 +1,6 @@
 public class fffffffffffffff {
     public static void main(String[] args) {
         System.out.println("ffffffffffff");
+        System.out.println("222222222222200000");
     }
 }
